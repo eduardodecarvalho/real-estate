c478d10269db32c96b6f7152156678fb17633a19
eduardo <eduardobcarvalho@outlook.com>
2020-12-23 16:24:14 -0300

    first commit

---
+++ src/main/java/com/api/realestate/RealEstateApplication.java
@@ -0,0 +1,13 @@
+package com.api.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateApplication.class, args);
+	}
+
+}
